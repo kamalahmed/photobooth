66f0fc7c25efe45e423d616ec07cc6b933fa4d5a
Kamal <kamalacca@gmail.com>
2024-04-07 15:33:05 +0400

    add photo details page

---
+++ app/[lang]/photos/[id]/page.js
@@ -0,0 +1,7 @@
+import PhotoDetails from "@/components/PhotoDetails";
+
+const PhotoDetailsPage = ({ params: { id, lang } }) => {
+  return <PhotoDetails id={id} lang={lang} />;
+};
+
+export default PhotoDetailsPage;

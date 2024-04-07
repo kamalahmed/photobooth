d4842dfb7b170b51811c61bc8bb26b2a455a0248
Kamal <kamalacca@gmail.com>
2024-04-07 15:25:55 +0400

    render all photos in the home

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -1,6 +1,8 @@
-import { getDictionary } from "./dictionaries";
+import PhotoList from "@/components/PhotoList";
 
-export default async function Home({ params: { lang } }) {
-  const dictionary = await getDictionary(lang);
-  return <div>PhotoBooth Home {dictionary.followers}</div>;
+export default async function Home() {
+  const response = await fetch(`${process.env.BASE_API_URL}/photos`);
+  const photos = await response.json();
+
+  return <PhotoList photos={photos} />;
 }
---
+++ components/PhotoList.jsx
@@ -0,0 +1,13 @@
+import PhotoCard from "./PhotoCard";
+
+const PhotoList = ({ photos }) => {
+  return (
+    <div className="img-grid">
+      {photos.map((photo) => (
+        <PhotoCard key={photo.id} photo={photo} />
+      ))}
+    </div>
+  );
+};
+
+export default PhotoList;
